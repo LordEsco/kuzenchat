b174b5e96dda4c891363bbdf97c3fb4a5cdd73c9
LordEsco <escotr1991@gmail.com>
2025-07-31 01:39:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(KuzenChatApp());
+
+class KuzenChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KuzenChat',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: Scaffold(
+        appBar: AppBar(title: Text('KuzenChat')),
+        body: Center(child: Text('Mesajlaşma Ekranı')),
+      ),
+    );
+  }
+}
